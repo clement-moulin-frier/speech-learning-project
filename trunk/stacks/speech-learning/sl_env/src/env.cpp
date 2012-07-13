6fb40a4d4e3cdd89ede6f288b1cdba13e9e54f30
patriotsfan@gmail.com <patriotsfan@gmail.com@5c60dbb7-622e-86ed-7680-fe7154259aad>
2012-07-13 14:05:22 +0000

    better setup for env node

--- trunk/stacks/speech-learning/sl_env/src/env.cpp
+++ trunk/stacks/speech-learning/sl_env/src/env.cpp
@@ -1,37 +1,8 @@
-#include <ros/ros.h>
-#include "std_msgs/String.h"
+#include "env.h"
 
-#include <sl_msgs/SLState.h>
-#include <sl_msgs/SLEnvDescription.h>
-#include <sl_msgs/SLAction.h>
-#include <sl_msgs/SLSpeech.h>
-
-#include <ros/callback_queue.h>
-#include <tf/transform_broadcaster.h>
-
-#include "Environment.hh"
-#include "Random.h"
-
-#include "SpeechArmTable.hh"
-
-#include <getopt.h>
-#include <stdlib.h>
 
 #define NODE "SLEnvironment"
 
-static ros::Publisher out_env_desc;
-static ros::Publisher out_state;
-
-Environment* e;
-Random rng;
-bool PRINTS = false;//true;
-int seed = 1;
-
-// some default parameters
-bool stochastic = true;
-int width = 5;
-int nobjects = 3;
-float speech_radius = 0.6;
 
 
 void displayHelp(){
